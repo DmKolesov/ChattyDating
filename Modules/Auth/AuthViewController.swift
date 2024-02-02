f36d5a7695d7e5122ff14166400c521ef3bbfbe9
Dima Kolesov <119799136+DmKolesov@users.noreply.github.com>
2024-02-02 17:04:33 +0300

    authModule

--- Modules/Auth/AuthViewController.swift
+++ Modules/Auth/AuthViewController.swift
@@ -1,6 +1,4 @@
 
-
-import Foundation
 import UIKit
 
 final class AuthViewController: UIViewController {
@@ -20,199 +18,25 @@ final class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupUI()
     }
 
     @objc
     func didTapForgorPassword() {
-        print("Forgot Password button tapped!")
+        print("Forgot Password button tapped")
     }
 
     @objc
     func didTapSignIn() {
-        print("didTapSignIn()")
+        print("SignIn button tapped")
     }
     @objc
     func didTapRegister() {
-        print("didTapRegister()")
+        print("Register button tapped")
     }
 }
 
-private extension AuthViewController {
-
-    func setupUI() {
-        setupGradientView()
-        setupLabel()
-        setupTextFields()
-
-        setupForgotPasswordButton()
-        setupSigniInButton()
-        setupRegisterButton()
-    }
-
-    func setupGradientView() {
-        gradientView.layer.cornerRadius = 50
-        gradientView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-        view.backgroundColor = .white
-        view.addSubview(gradientView)
-      
-        gradientView.translatesAutoresizingMaskIntoConstraints = false
-
-        let heightMultiplier: CGFloat = 0.7
-        
-        // завивисимость от устройства юай девайс каррент
-        // градиент бабл
-        
-        // register aligment label textfield text
-
-        let heightConstraint = gradientView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: heightMultiplier)
-        heightConstraint.priority = .defaultHigh
-        heightConstraint.isActive = true
-
-        NSLayoutConstraint.activate([
-            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            gradientView.topAnchor.constraint(equalTo: view.topAnchor)
-        ])
-
-    }
-
-    func setupLabel() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.font = StyleConstants.Font.largeTitle
-        titleLabel.numberOfLines = 2
-        titleLabel.textColor = StyleConstants.Colors.TextColor.titleColor
-        titleLabel.text = TextConstants.brandName
-        
-        gradientView.addSubview(titleLabel)
-
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: gradientView.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: gradientView.topAnchor, constant: view.bounds.height * 0.15),
-            titleLabel.widthAnchor.constraint(lessThanOrEqualToConstant: view.bounds.width * 0.8)
-        ])
-    }
-
-    func setupTextFields() {
-        emailTextFieldView = CustomTextFieldView(textField: emailTextField, placeholder: TextConstants.emailPhonePlaceholder)
-        passwordTextFieldView = CustomTextFieldView(textField: passwordTextField, placeholder: TextConstants.passwordPlaceHolder, isPassword: true)
-
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
-
-        let stackView = UIStackView(arrangedSubviews: [emailTextFieldView, passwordTextFieldView])
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-
-        let stackViewTopConstraint = stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.bounds.height * 0.09)
-        stackViewTopConstraint.priority = .required
-        stackViewTopConstraint.isActive = true
-
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 40),
-            stackView.trailingAnchor.constraint(equalTo: gradientView.trailingAnchor, constant: -40)
-        ])
-    }
-    
-    func setupForgotPasswordButton() {
-        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
-        forgotPasswordButton.setTitle(TextConstants.forgotPasswordButton, for: .normal)
-        forgotPasswordButton.titleLabel?.font = StyleConstants.Font.placeholderTitle
-        forgotPasswordButton.setTitleColor(StyleConstants.Colors.TextColor.titleColor, for: .normal)
-        forgotPasswordButton.addTarget(self, action: #selector(didTapForgorPassword), for: .touchUpInside)
-
-        gradientView.addSubview(forgotPasswordButton)
-        NSLayoutConstraint.activate([
-            forgotPasswordButton.topAnchor.constraint(equalTo: passwordTextFieldView.bottomAnchor, constant: 8),
-            forgotPasswordButton.trailingAnchor.constraint(equalTo: gradientView.trailingAnchor, constant: -40),
-        ])
-    }
-
-    func setupSigniInButton() {
-        signInButton.translatesAutoresizingMaskIntoConstraints = false
-        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
-
-        let backGroundViewSignInButton: BackgroundViewButton = {
-            let view = BackgroundViewButton()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.layer.cornerRadius = 20
-            view.clipsToBounds = true
-        
-            return view
-        }()
-
-        let labelSignIn: UILabel = {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.font = StyleConstants.Font.buttonTitle
-            label.textColor = StyleConstants.Colors.TextColor.titleColor
-            label.text = TextConstants.signInButton
-            return label
-        }()
-
-        backGroundViewSignInButton.addSubview(labelSignIn)
-        backGroundViewSignInButton.addSubview(signInButton)
-        view.addSubview(backGroundViewSignInButton)
-
-        NSLayoutConstraint.activate([
-            backGroundViewSignInButton.topAnchor.constraint(equalTo: gradientView.bottomAnchor, constant: 60),
-            backGroundViewSignInButton.centerXAnchor.constraint(equalTo: gradientView.centerXAnchor),
-            backGroundViewSignInButton.widthAnchor.constraint(equalToConstant: 195),
-            backGroundViewSignInButton.heightAnchor.constraint(equalToConstant: 40),
-
-            labelSignIn.centerXAnchor.constraint(equalTo: backGroundViewSignInButton.centerXAnchor),
-            labelSignIn.centerYAnchor.constraint(equalTo: backGroundViewSignInButton.centerYAnchor),
-            labelSignIn.heightAnchor.constraint(equalToConstant: 20),
-
-            signInButton.topAnchor.constraint(equalTo: backGroundViewSignInButton.topAnchor),
-            signInButton.leadingAnchor.constraint(equalTo: backGroundViewSignInButton.leadingAnchor),
-            signInButton.trailingAnchor.constraint(equalTo: backGroundViewSignInButton.trailingAnchor),
-            signInButton.bottomAnchor.constraint(equalTo: backGroundViewSignInButton.bottomAnchor)
-        ])
-
-    }
-
-    func setupRegisterButton() {
-
-        registerButton.translatesAutoresizingMaskIntoConstraints = false
-        registerButton.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
-        registerButton.clipsToBounds = true
-        registerButton.cornerRadius = 20
-
-        let gradientLabel: UILabel = {
-            let label = GradientLabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-      
-            label.layer.cornerRadius = 20
-            label.clipsToBounds = true
-            label.font = StyleConstants.Font.buttonTitle
-            label.text = TextConstants.registerButton
-            label.textAlignment = .center
-            
-        
-            return label
-        }()
-
-        view.addSubview(gradientLabel)
-        view.addSubview(registerButton)
-
-        NSLayoutConstraint.activate([
-
-            registerButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 10),
-            registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            registerButton.widthAnchor.constraint(equalToConstant: 195),
-            registerButton.heightAnchor.constraint(equalToConstant: 40),
-
-            gradientLabel.topAnchor.constraint(equalTo: registerButton.topAnchor, constant: 10),
-            gradientLabel.leadingAnchor.constraint(equalTo: registerButton.leadingAnchor, constant: 10),
-            gradientLabel.trailingAnchor.constraint(equalTo: registerButton.trailingAnchor, constant: -10),
-            gradientLabel.bottomAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: -10)
-
-        ])
-    }
-}
 extension AuthViewController: UITextFieldDelegate {
     // MARK: - UITextFieldDelegate
     
@@ -223,5 +47,156 @@ extension AuthViewController: UITextFieldDelegate {
             print("Password Text: \(textField.text ?? "")")
         }
         return true
+    }
+}
+
+private extension AuthViewController {
+    func setupUI() {
+        setupGradientView()
+        setupLabel()
+        setupTextFields()
+        setupForgotPasswordButton()
+        setupSigniInButton()
+        setupRegisterButton()
+    }
+    func setupGradientView() {
+        gradientView.layer.cornerRadius = AuthModuleLayoutConstants.gradientViewCornerRadius
+        gradientView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gradientView)
+      
+        NSLayoutConstraint.activate([
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientView.heightAnchor.constraint(equalToConstant: view.bounds.height / AuthModuleLayoutConstants.gradientViewHeightRatio),
+            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
+    func setupLabel() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = StyleConstants.Font.largeTitle
+        titleLabel.numberOfLines = 2
+        titleLabel.textColor = StyleConstants.Colors.TextColor.titleColor
+        titleLabel.text = TextConstants.brandName
+        gradientView.addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: gradientView.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: gradientView.topAnchor, constant: view.bounds.width * AuthModuleLayoutConstants.titleLabelTopOffsetRatio),
+            titleLabel.widthAnchor.constraint(lessThanOrEqualToConstant: view.bounds.width * AuthModuleLayoutConstants.titleLabelWidthRatio)
+        ])
+    }
+    func setupTextFields() {
+        emailTextFieldView = CustomTextFieldView(textField: emailTextField, placeholder: TextConstants.emailPhonePlaceholder)
+        passwordTextFieldView = CustomTextFieldView(textField: passwordTextField, placeholder: TextConstants.passwordPlaceHolder, isPassword: true)
+
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
+        let stackView = UIStackView(arrangedSubviews: [emailTextFieldView, passwordTextFieldView])
+        stackView.axis = .vertical
+        stackView.spacing = AuthModuleLayoutConstants.textFieldSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        let stackViewTopConstraint = stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: view.bounds.height * AuthModuleLayoutConstants.textFieldTopOffsetRatio)
+        stackViewTopConstraint.priority = .required
+        stackViewTopConstraint.isActive = true
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: AuthModuleLayoutConstants.stackViewAnchor),
+            stackView.trailingAnchor.constraint(equalTo: gradientView.trailingAnchor, constant: -AuthModuleLayoutConstants.stackViewAnchor)
+        ])
+    }
+    func setupForgotPasswordButton() {
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.setTitle(TextConstants.forgotPasswordButton, for: .normal)
+        forgotPasswordButton.titleLabel?.font = StyleConstants.Font.placeholderTitle
+        forgotPasswordButton.setTitleColor(StyleConstants.Colors.TextColor.titleColor, for: .normal)
+        forgotPasswordButton.addTarget(self, action: #selector(didTapForgorPassword), for: .touchUpInside)
+
+        gradientView.addSubview(forgotPasswordButton)
+        NSLayoutConstraint.activate([
+            forgotPasswordButton.topAnchor.constraint(equalTo: passwordTextFieldView.bottomAnchor, constant: AuthModuleLayoutConstants.forgotPasswordButtonTopOffset),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: passwordTextFieldView.trailingAnchor),
+        ])
+    }
+    func setupSigniInButton() {
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+
+        let backGroundViewSignInButton: BackgroundViewButton = {
+            let view = BackgroundViewButton()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.layer.cornerRadius = AuthModuleLayoutConstants.cornerRadius
+            view.clipsToBounds = true
+        
+            return view
+        }()
+
+        let labelSignIn: UILabel = {
+            let view = UILabel()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.font = StyleConstants.Font.buttonTitle
+            view.textColor = StyleConstants.Colors.TextColor.titleColor
+            view.text = TextConstants.signInButton
+            
+            return view
+        }()
+        
+        backGroundViewSignInButton.addSubview(labelSignIn)
+        backGroundViewSignInButton.addSubview(signInButton)
+        view.addSubview(backGroundViewSignInButton)
+
+        NSLayoutConstraint.activate([
+            backGroundViewSignInButton.topAnchor.constraint(equalTo: gradientView.bottomAnchor, constant: AuthModuleLayoutConstants.signInButtonTopOffset),
+            backGroundViewSignInButton.centerXAnchor.constraint(equalTo: gradientView.centerXAnchor),
+            backGroundViewSignInButton.widthAnchor.constraint(equalToConstant: AuthModuleLayoutConstants.authButtonWidth),
+            backGroundViewSignInButton.heightAnchor.constraint(equalToConstant: AuthModuleLayoutConstants.authButtonHeight),
+
+            labelSignIn.centerXAnchor.constraint(equalTo: backGroundViewSignInButton.centerXAnchor),
+            labelSignIn.centerYAnchor.constraint(equalTo: backGroundViewSignInButton.centerYAnchor),
+            labelSignIn.heightAnchor.constraint(equalToConstant: AuthModuleLayoutConstants.labelOffset),
+
+            signInButton.topAnchor.constraint(equalTo: backGroundViewSignInButton.topAnchor),
+            signInButton.leadingAnchor.constraint(equalTo: backGroundViewSignInButton.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: backGroundViewSignInButton.trailingAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: backGroundViewSignInButton.bottomAnchor)
+        ])
+
+    }
+    func setupRegisterButton() {
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.addTarget(self, action: #selector(didTapRegister), for: .touchUpInside)
+        registerButton.clipsToBounds = true
+        registerButton.cornerRadius = AuthModuleLayoutConstants.cornerRadius
+
+        let gradientLabel: UILabel = {
+            let view = GradientLabel()
+            view.translatesAutoresizingMaskIntoConstraints = false
+      
+            view.layer.cornerRadius = AuthModuleLayoutConstants.cornerRadius
+            view.clipsToBounds = true
+            view.font = StyleConstants.Font.buttonTitle
+            view.text = TextConstants.registerButton
+            view.textAlignment = .center
+            
+            return view
+        }()
+
+        view.addSubview(gradientLabel)
+        view.addSubview(registerButton)
+
+        NSLayoutConstraint.activate([
+
+            registerButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: AuthModuleLayoutConstants.buttonOffset),
+            registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            registerButton.widthAnchor.constraint(equalToConstant: AuthModuleLayoutConstants.authButtonWidth),
+            registerButton.heightAnchor.constraint(equalToConstant: AuthModuleLayoutConstants.authButtonHeight),
+
+            gradientLabel.topAnchor.constraint(equalTo: registerButton.topAnchor, constant: AuthModuleLayoutConstants.buttonOffset),
+            gradientLabel.leadingAnchor.constraint(equalTo: registerButton.leadingAnchor, constant: AuthModuleLayoutConstants.buttonOffset),
+            gradientLabel.trailingAnchor.constraint(equalTo: registerButton.trailingAnchor, constant: -AuthModuleLayoutConstants.buttonOffset),
+            gradientLabel.bottomAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: -AuthModuleLayoutConstants.buttonOffset)
+        ])
     }
 }
---
+++ Resourses/LayoutConstants.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+enum AuthModuleLayoutConstants {
+    static let gradientViewCornerRadius: CGFloat = 50
+    static let cornerRadius: CGFloat = 20
+    static let gradientViewHeightRatio: CGFloat = 1.48
+    static let titleLabelTopOffsetRatio: CGFloat = 0.15
+    static let titleLabelWidthRatio: CGFloat = 0.8
+    static let forgotPasswordButtonTopOffset: CGFloat = 8
+    static let signInButtonTopOffset: CGFloat = 60
+    static let authButtonWidth: CGFloat = 195
+    static let authButtonHeight: CGFloat = 40
+    static let buttonOffset: CGFloat = 10
+    static let textFieldTopOffsetRatio: CGFloat = 0.09
+    static let stackViewAnchor: CGFloat = 40
+    static let textFieldSpacing: CGFloat = 20
+    static let labelOffset: CGFloat = 20
+}
